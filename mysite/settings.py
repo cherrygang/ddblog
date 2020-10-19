58808cbd149402787b3545faa214dba81db0c885
Gary <chang324@gmail.com>
2020-10-19 15:09:41 -0400

    ready for heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,11 +122,17 @@ USE_L10N = True
 
 USE_TZ = True
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find statif files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static')    
+)
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
@@ -138,3 +144,5 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 #settings for heroku
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
