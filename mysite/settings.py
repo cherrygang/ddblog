4a0ff1629b0a1e643e54b653a3d34c24a71b1311
Gary <chang324@gmail.com>
2020-10-19 16:31:41 -0400

    troubleshoot heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -142,4 +142,4 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
