7173f9273cb0273e4b5c9131e416f46b01b03257
Gary <chang324@gmail.com>
2020-10-19 21:30:22 -0400

    adding logging

--- mysite/settings.py
+++ mysite/settings.py
@@ -147,3 +147,20 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Activate Django-Heroku
 django_heroku.settings(locals())
+
+#Logging for heroku error
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
